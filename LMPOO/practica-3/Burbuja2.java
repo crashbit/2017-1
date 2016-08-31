a292bc92497fbba4dc0f0b0e05d3fea517f307a5
Germán Santos Jaimes <germans@unam.mx>
2016-08-31 16:30:37 -0500

    Burbuja con paso de parametros

--- LMPOO/practica-3/Burbuja2.java
+++ LMPOO/practica-3/Burbuja2.java
@@ -5,20 +5,39 @@ public class Burbuja2{
 	public static void main(String args[]){
 		int i, j, temp;
 		int[] x;
+		int flag;
+		String orden ="mayor";
 
-		x = new int[args.length];
+		x = new int[args.length-1];
 
-		for(i = 0; i<args.length; i++){
+		for(i = 0; i<args.length-1; i++){
 			x[i] = Integer.parseInt(args[i]);
 		}
+
+		if(orden.compareTo(args[args.length-1]) == 0){
+			flag = 0;
+		}
+		else{
+			flag = 1;
+		}
+
 	
-		for(i=0; i< args.length;i++){
-			for(j=i+1; j< args.length; j++){
-				if(x[i]>x[j]){
+		for(i=0; i< args.length-1;i++){
+			for(j=i+1; j< args.length-1; j++){
+				if(flag==1){
+					if(x[i]>x[j]){
+						temp = x[i];
+						x[i] = x[j];
+						x[j] = temp;
+					}
+				}
+				else{
+					if(x[i]<x[j]){
 					temp = x[i];
 					x[i] = x[j];
 					x[j] = temp;
-				}
+					}
+				}	
 			}
 		}
 
